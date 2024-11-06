93bdf3f93561487cc2af1179620d8cfc240e2e51
Marria Rybak <marjarybak@gmail.com>
2024-11-06 16:47:37 +0100

    Add profile-menu and log out

--- TaskPlanner/Models/LoginViewModel.cs
+++ TaskPlanner/Models/LoginViewModel.cs
@@ -4,11 +4,11 @@ public class LoginViewModel
 {
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Invalid email format.")]
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
     [Required(ErrorMessage = "Password is required.")]
     [DataType(DataType.Password)]
-    public string Password { get; set; }
+    public required string Password { get; set; }
 
-    public bool RememberMe { get; set; }
+    public required bool RememberMe { get; set; }
 }
--- TaskPlanner/Models/RegisterViewModel.cs
+++ TaskPlanner/Models/RegisterViewModel.cs
@@ -4,14 +4,14 @@ public class RegisterViewModel
 {
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Invalid email format.")]
-    public string Email { get; set; }
+    public required string Email { get; set; }
 
     [Required(ErrorMessage = "Password is required.")]
     [DataType(DataType.Password)]
-    public string Password { get; set; }
+    public required string Password { get; set; }
 
     [Required(ErrorMessage = "Confirm Password is required.")]
     [DataType(DataType.Password)]
     [Compare("Password", ErrorMessage = "Passwords do not match.")]
-    public string ConfirmPassword { get; set; }
+    public required string ConfirmPassword { get; set; }
 }
